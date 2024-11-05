e0b7e7d688d41042409cbbb58aa79931fb54a030
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-11-05 16:11:59 +0000

    About page

--- frontend/components/about/AboutPage/index.tsx
+++ frontend/components/about/AboutPage/index.tsx
@@ -3,108 +3,120 @@
 /* * */
 
 import { Surface } from '@/components/layout/Surface';
-import { Accordion } from '@mantine/core';
+import { Accordion, Image, List, Table } from '@mantine/core';
+import { useTranslations } from 'next-intl';
+
+import styles from './styles.module.css';
 
 /* * */
 
 export function AboutPage() {
+	//
+
+	//
+	// A. Setup variables
+
+	const t = useTranslations('about.AboutPage');
+
+	//
+	// B. Render components
+
 	return (
 		<>
 
 			<Surface forceOverflow>
+				<Image alt="Carris Metropolitana" src="/images/about/about-header.svg" />
+			</Surface>
+
+			<Surface forceOverflow>
 				<Accordion>
 
-					<Accordion.Item value="a">
-						<Accordion.Control><p>A nossa jornada na Mobilidade Metropolitana</p></Accordion.Control>
+					<Accordion.Item value="section-1">
+						<Accordion.Control>{t('sections.section-1.title')}</Accordion.Control>
 						<Accordion.Panel>
-							<p>Em 2019, na vasta área metropolitana de Lisboa (amL), surgiu um desafio: unificar os serviços de transporte de 18 municípios, garantindo que cada passageiro tivesse acesso a um transporte público de qualidade, eficiente e sustentável. Foi com este objetivo que nasceu a Carris Metropolitana (CMetropolitana), uma marca única dedicada a proporcionar, de igual forma, um bom sistema de mobilidade em todos os municípios da amL.</p>
-							<p>Este foi o nascimento de um gigante amarelo, uma vez que a operação da CMetropolitana tem uma dimensão única a nível nacional, cobrindo mais de 3 mil quilômetros quadrados e fornecendo serviço em 12.600 paragens. Em 2023, a marca implementou melhorias significativas, incluindo ajustes nos horários e percursos, resultando em mais de 20% de aumento no serviço e mais de 30% na quilometragem em comparação com o ano anterior. Esses esforços refletiram-se no total de 141,5 milhões de passageiros transportados ao longo do ano.</p>
+							<p className={styles.text}>{t('sections.section-1.text-1')}</p>
+							<p className={styles.text}>{t('sections.section-1.text-2')}</p>
 						</Accordion.Panel>
 					</Accordion.Item>
 
-					<Accordion.Item value="b">
-						<Accordion.Control><p>O que existia antes da CMetropolitana?</p></Accordion.Control>
+					<Accordion.Item value="section-2">
+						<Accordion.Control>{t('sections.section-2.title')}</Accordion.Control>
 						<Accordion.Panel>
-							<p>Até ao nascimento do gigante amarelo, isto é, até à entrada da Carris Metropolitana em operação, existiam operadores privados distintos, focados no serviço rodoviário de diferentes municípios.  Nesta realidade, o passageiro precisava de conhecer as diferentes operações dos diferentes municípios, para poder circular pela amL. Sem uma unificação na informação ao público, a consulta de horários, percursos e tarifas não era igual para todas as zonas. Ainda, a rede era relativamente díspar entre os diferentes municípios, com a existência de áreas sem qualquer tipo de serviço rodoviário.</p>
+							<p className={styles.text}>{t('sections.section-2.text-1')}</p>
+							<p className={styles.text}>{t('sections.section-2.text-2')}</p>
+							<p className={styles.text}>{t('sections.section-2.text-3')}</p>
 						</Accordion.Panel>
 					</Accordion.Item>
 
-					<Accordion.Item value="c">
-						<Accordion.Control><p>O que é que a Carris Metropolitana trouxe?</p></Accordion.Control>
+					<Accordion.Item value="section-3">
+						<Accordion.Control>{t('sections.section-3.title')}</Accordion.Control>
 						<Accordion.Panel>
-							<p>A chegada da CMetropolitana possibilitou, sobretudo, uma unificação da experiência do passageiro nos diferentes municípios, com uma única imagem e nível de serviço ao longo de toda a amL. A consulta de horários, percursos, tarifas, ou qualquer outra informação passou a ser feita de forma igual por todo o terreno e a cor amarela chegou a lugares que antes não tinham qualquer tipo de serviço. Houve, ainda, uma renovação dos veículos para uma frota com uma idade média de menos de um ano (em 2023), com 5 % de veículos não poluentes e energeticamente eficientes e 90 % de veículos com classe de emissões EURO V ou superior. Para além disso, todos os veículos passaram a ser acessíveis. A CMetropolitana, desde a sua chegada continua a bater recordes: no dia 8 de maio de 2023 bateu o recorde de 650 mil passageiros transportados num dia útil e, este ano, no dia 17 de setembro, chegámos aos 670 mil passageiros transportados.</p>
+							<p className={styles.text}>{t('sections.section-3.text-1')}</p>
+							<p className={styles.text}>{t('sections.section-3.text-2')}</p>
+							<p className={styles.text}>{t('sections.section-3.text-3')}</p>
 						</Accordion.Panel>
 					</Accordion.Item>
 
-					<Accordion.Item value="d">
-						<Accordion.Control><p>Estrutura</p></Accordion.Control>
+					<Accordion.Item value="section-4">
+						<Accordion.Control>{t('sections.section-4.title')}</Accordion.Control>
 						<Accordion.Panel>
-							<p>O projeto Carris Metropolitana é uma iniciativa dos 18 municípios da área metropolitana de Lisboa (amL) que, através da TML - Transportes Metropolitanos de Lisboa, funde a operação de transporte público rodoviário de toda a amL numa só imagem e serviço. A amL - área metropolitana de Lisboa é uma enti¬dade pública composta pelos 18 municípios perten¬centes à área metropolitana de Lisboa. Entre outras atribuições, a amL é também a autoridade de trans¬porte público municipal e intermunicipal da região, por delegação de competências de 15 dos 18 municípios. A TML - Transportes Metropolitanos de Lisboa é uma entidade de gestão pública responsável pela planificação das tarifas e zonas tarifárias do transporte público da área metropolitana de Lisboa. A TML tem um estatuto de dependência da amL - área metropolitana de Lisboa e detém duas mar¬cas; a Carris Metropolitana e a navegante®.</p>
-							<h2>IMAGEM</h2>
+							<p className={styles.text}>{t('sections.section-4.text-1')}</p>
+							<p className={styles.text}>{t('sections.section-4.text-2')}</p>
+							<p className={styles.text}>{t('sections.section-4.text-3')}</p>
+							<p className={styles.text}>{t('sections.section-4.text-4')}</p>
+							<Image alt="Carris Metropolitana" src="/images/about/about-estrutura.svg" />
 						</Accordion.Panel>
 					</Accordion.Item>
 
-					<Accordion.Item value="e">
-						<Accordion.Control><p>As áreas da Carris Metropolitana</p></Accordion.Control>
+					<Accordion.Item value="section-5">
+						<Accordion.Control>{t('sections.section-5.title')}</Accordion.Control>
 						<Accordion.Panel>
-							<p>A Carris Metropolitana espalhou-se por 15 dos 18 municípios da amL, dividindo as suas operações em quatro áreas geográficas, cada uma com um operador dedicado, com um contrato de serviço público.</p>
-							<h2>IMAGEM</h2>
-							Área 1:
-							Operador: Viação Alvorada
-							Municípios: Amadora, Oeiras, Sintra, Cascais* e Lisboa*.
-
-							Área 2:
-							Operador: Rodoviária de Lisboa
-							Municípios: Mafra, Vila Franca de Xira, Odivelas e Loures
-
-							Área 3:
-							Operador: TST
-							Municípios: Almada, Seixal e Sesimbra.
-
-							Área 4:
-							Operador: Alsa Todi
-							Municípios: Alcochete, Moita, Montijo, Palmela, Setúbal e Barreiro*.
-
-							*Apenas serviço intermunicipal.Nos municípios do Barreiro, Cascais e Lisboa, a operação local é realizada por operadores próprios - os TCB, MobiCascais e Carris, respetivamente. No entanto, essas cidades também estão ligadas através das ligações intermunicipais da Carris Metropolitana, garantindo integração entre as diferentes redes de transporte.
-
-							Isto significa que, onde quer que esteja, há um autocarro amarelo que liga a sua cidade a diversos destinos, simplificando os percursos e tornando as viagens entre municípios fáceis e tranquilas.
+							<p className={styles.text}>{t('sections.section-5.text-1')}</p>
+							<Table
+								my={20}
+								data={{
+									body: [
+										[t('sections.section-5.table.rows.tr-1.tc-1'), t('sections.section-5.table.rows.tr-1.tc-2'), t('sections.section-5.table.rows.tr-1.tc-3')],
+										[t('sections.section-5.table.rows.tr-2.tc-1'), t('sections.section-5.table.rows.tr-2.tc-2'), t('sections.section-5.table.rows.tr-2.tc-3')],
+										[t('sections.section-5.table.rows.tr-3.tc-1'), t('sections.section-5.table.rows.tr-3.tc-2'), t('sections.section-5.table.rows.tr-3.tc-3')],
+										[t('sections.section-5.table.rows.tr-4.tc-1'), t('sections.section-5.table.rows.tr-4.tc-2'), t('sections.section-5.table.rows.tr-4.tc-3')],
+									],
+									caption: t('sections.section-5.table.caption'),
+									head: [t('sections.section-5.table.head.th-1'), t('sections.section-5.table.head.th-2'), t('sections.section-5.table.head.th-3')],
+								}}
+							/>
+							<p className={styles.text}>{t('sections.section-5.text-2')}</p>
+							<p className={styles.text}>{t('sections.section-5.text-3')}</p>
 						</Accordion.Panel>
 					</Accordion.Item>
 
-					<Accordion.Item value="f">
-						<Accordion.Control><p>Inovação e sustentabilidade</p></Accordion.Control>
+					<Accordion.Item value="section-6">
+						<Accordion.Control>{t('sections.section-6.title')}</Accordion.Control>
 						<Accordion.Panel>
-							Mas a Carris Metropolitana não se resume apenas a autocarros. A marca investiu também na sustentabilidade, reduzindo a idade média da sua frota para menos de um ano (em 2023), e somando 5% de veículos não poluentes ou energeticamente eficientes. Como medidas de redução de emissões poluentes, a Carris Metropolitana ainda estabeleceu que um mínimo de 5% (cinco por cento) dos veículos devem estar preparados para o transporte de bicicletas, e estabeleceu uma frota com 90 % de veículos com classe de emissões EURO V ou superior, com redução de ruído no interior e exterior do veículo e baixos consumos energéticos. Estes são termos estabelecidos nos contratos de serviço público celebrados com os operadores.
-
-							A nível tecnológico, a criação da Carris Metropolitana prevê:
-							•	Inclusão de serviços de entretenimento/informação e Wi-Fi a bordo
-							•	Eco-condução, aumento de oferta e promoção da pontualidade
-							•	Planeamento e ajuste do serviço
-							•	Aumento substancial da rede de vendas e serviços de apoio ao passageiro
-							•	Integração tecnológica com diferentes plataformas (Google Maps, apple maps e moovit)
-							•	Informação em tempo real do tempo de espera dos autocarros e das filas dos Espaços navegante® Carris Metropolitana
-							•	Uma política de dados abertos
-							•	Criação de uma aplicação móvel
-
-							Além disso, a CMetropolitana trouxe os Painéis de Informação ao Público (PIP) e os MUPIs. Os primeiros tratam-se de painéis estrategicamente localizados que fornecem informações em tempo real sobre a chegada dos autocarros, permitindo que os passageiros planeiem as suas viagens com total confiança. Atualmente, estão instalados 210 painéis. Os MUPIS representam outra forma de informação ao público, uma mais interativa, que fornece informações de rede, como diagramas, horários e percursos.
-
-							[imagem]
+							<p className={styles.text}>{t('sections.section-6.text-1')}</p>
+							<p className={styles.text}>{t('sections.section-6.text-2')}</p>
+							<p className={styles.text}>{t('sections.section-6.text-3')}</p>
+							<p className={styles.text}>{t('sections.section-6.text-4')}</p>
+							<List mt={10} size="lg" withPadding>
+								<List.Item>{t('sections.section-6.list.item-1')}</List.Item>
+								<List.Item>{t('sections.section-6.list.item-2')}</List.Item>
+								<List.Item>{t('sections.section-6.list.item-3')}</List.Item>
+								<List.Item>{t('sections.section-6.list.item-4')}</List.Item>
+								<List.Item>{t('sections.section-6.list.item-5')}</List.Item>
+								<List.Item>{t('sections.section-6.list.item-6')}</List.Item>
+								<List.Item>{t('sections.section-6.list.item-7')}</List.Item>
+								<List.Item>{t('sections.section-6.list.item-8')}</List.Item>
+							</List>
+							<p className={styles.text}>{t('sections.section-6.text-5')}</p>
 						</Accordion.Panel>
 					</Accordion.Item>
 
-					<Accordion.Item value="g">
-						<Accordion.Control><p>Uma identidade que reflete a mobilidade</p></Accordion.Control>
+					<Accordion.Item value="section-7">
+						<Accordion.Control>{t('sections.section-7.title')}</Accordion.Control>
 						<Accordion.Panel>
-							<p>Com um visual vibrante e reconhecível, a identidade visual da Carris Metropolitana é mais do que a sua estética. As cores, a tipografia e os elementos gráficos foram projetados para permitir uma leitura homogénea e também clara da rede, permitindo uma experiência consistente dos passageiros da amL. Seja nas redes sociais, nas paragens ou nas lojas Espaços navegante® Carris Metropolitana, a comunicação é uniforme, clara e acessível, garantindo que todos, de qualquer parte da amL, se sintam parte desta transformação na mobilidade.</p>
-						</Accordion.Panel>
-					</Accordion.Item>
-
-					<Accordion.Item value="h">
-						<Accordion.Control><p>O nosso compromisso</p></Accordion.Control>
-						<Accordion.Panel>
-							<p>Hoje, a Carris Metropolitana continua a evoluir, sempre com o olhar no futuro e um compromisso firme: oferecer um transporte público que não seja apenas eficiente, mas também sustentável e justo. A nossa missão é manter os municípios conectados, aproximar os passageiros e transformar a deslocação numa experiência mais simples e prazerosa para todos.</p>
-							<p>A CMetropolitana reitera o seu compromisso em fornecer um transporte público seguro, eficiente e acessível para todos os habitantes da área metropolitana de Lisboa e agradece a colaboração contínua e o apoio dos passageiros na realização desse objetivo comum.</p>
-							<p>CMetropolitana, é de todos!</p>
+							<p className={styles.text}>{t('sections.section-7.text-1')}</p>
+							<p className={styles.text}>{t('sections.section-7.text-2')}</p>
+							<p className={styles.text}>{t('sections.section-7.text-3')}</p>
 						</Accordion.Panel>
 					</Accordion.Item>
 
