001189017127104ac39cba4b6d8f994692bce965
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-12-03 10:38:59 +0000

    Xmas Launch

--- frontend/components/header/Logo/index.tsx
+++ frontend/components/header/Logo/index.tsx
@@ -1,32 +1,19 @@
-// 'use client';
+'use client';
 
 /* * */
 
 import { ThemeSwitch } from '@/components/responsive/ThemeSwitch';
-import { BrandsCmet } from '@/settings/assets.settings';
-import { Image } from '@mantine/core';
-// import Lottie from 'lottie-react';
+// import { BrandsCmet } from '@/settings/assets.settings';
+// import { Image } from '@mantine/core';
+import Lottie from 'lottie-react';
 import Link from 'next/link';
 
 import styles from './styles.module.css';
 
 /* * */
 
-// import xmasDarkAnimation from '@/assets/animations/header/xmas/xmas-dark.json';
-// import xmasLightAnimation from '@/assets/animations/header/xmas/xmas-light.json';
-
-/* * */
-
-// export function Logo() {
-// 	return (
-// 		<Link className={styles.container} href="/">
-// 			<ThemeSwitch
-// 				dark={<Lottie animationData={xmasDarkAnimation} style={{ height: 70, width: 150 }} />}
-// 				light={<Lottie animationData={xmasLightAnimation} style={{ height: 70, width: 150 }} />}
-// 			/>
-// 		</Link>
-// 	);
-// }
+import xmasDarkAnimation from '@/assets/animations/header/xmas/xmas-dark.json';
+import xmasLightAnimation from '@/assets/animations/header/xmas/xmas-light.json';
 
 /* * */
 
@@ -34,9 +21,22 @@ export function Logo() {
 	return (
 		<Link className={styles.container} href="/">
 			<ThemeSwitch
-				dark={<Image alt="Carris Metropolitana" src={BrandsCmet.cmet_dark} style={{ height: 40, width: 150 }} />}
-				light={<Image alt="Carris Metropolitana" src={BrandsCmet.cmet_light} style={{ height: 40, width: 150 }} />}
+				dark={<Lottie animationData={xmasDarkAnimation} style={{ height: 70, width: 150 }} />}
+				light={<Lottie animationData={xmasLightAnimation} style={{ height: 70, width: 150 }} />}
 			/>
 		</Link>
 	);
 }
+
+/* * */
+
+// export function Logo() {
+// 	return (
+// 		<Link className={styles.container} href="/">
+// 			<ThemeSwitch
+// 				dark={<Image alt="Carris Metropolitana" src={BrandsCmet.cmet_dark} style={{ height: 40, width: 150 }} />}
+// 				light={<Image alt="Carris Metropolitana" src={BrandsCmet.cmet_light} style={{ height: 40, width: 150 }} />}
+// 			/>
+// 		</Link>
+// 	);
+// }
