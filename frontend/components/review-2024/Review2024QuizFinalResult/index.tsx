66c3536e834dc50a4bc7654983d0d569a9e50a5f
Francisco <francisco.neto@tmlmobilidade.pt>
2025-01-14 18:35:21 +0000

    Upade Review2024: Quiz final results Gifs

--- frontend/components/review-2024/Review2024QuizFinalResult/index.tsx
+++ frontend/components/review-2024/Review2024QuizFinalResult/index.tsx
@@ -31,11 +31,11 @@ export function Review2024QuizFinalResult({ points }: Props) {
 	const totalPoints = allQuizData.reduce((acc, quiz) => acc + quiz._points, 0);
 
 	const pointTiers = [
-		{ _id: 'terrible', value: totalPoints * 0.2, animation: '/assets/review-2024/animations/digital/website_daily.json' },
-		{ _id: 'bad', value: totalPoints * 0.4, animation: '/assets/review-2024/animations/digital/website_daily.json' },
-		{ _id: 'good', value: totalPoints * 0.6, animation: '/assets/review-2024/animations/digital/website_daily.json' },
-		{ _id: 'incredible', value: totalPoints - 1, animation: '/assets/review-2024/animations/digital/website_daily.json' },
-		{ _id: 'perfect', value: totalPoints, animation: '/assets/review-2024/animations/digital/website_daily.json' },
+		{ _id: 'terrible', value: totalPoints * 0.2, animation: '/assets/review-2024/animations/quiz/quiz_finalresult_terrible.json' },
+		{ _id: 'bad', value: totalPoints * 0.4, animation: '/assets/review-2024/animations/quiz/quiz_finalresult_bad.json' },
+		{ _id: 'good', value: totalPoints * 0.6, animation: '/assets/review-2024/animations/quiz/quiz_finalresult_good.json' },
+		{ _id: 'incredible', value: totalPoints - 1, animation: '/assets/review-2024/animations/quiz/quiz_finalresult_incredible.json' },
+		{ _id: 'perfect', value: totalPoints, animation: '/assets/review-2024/animations/quiz/quiz_finalresult_perfect.json' },
 	];
 
 	const pointTier = pointTiers.find(tier => tier.value >= points) || pointTiers[0];
