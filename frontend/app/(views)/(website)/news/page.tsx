4ed8b3be930d1883c9c3d2c3bca76b6b3c8bd879
Skasix00 <brunoalexcastelo@gmail.com>
2025-01-24 19:57:35 +0000

    Added Dynamic page links to news page

--- frontend/app/(views)/(website)/news/page.tsx
+++ frontend/app/(views)/(website)/news/page.tsx
@@ -7,3 +7,11 @@ import { NewsList } from '@/components/news/NewsList';
 export default function Page() {
 	return <NewsList />;
 }
+export const metadata: Metadata = {
+	description: 'Todas as noticias da carris metropolitana',
+	openGraph: {
+		description: 'Notícias',
+		title: 'CMetropolitana - Notícias',
+	},
+	title: 'CMetropolitana - Notícias',
+};
