cc648a763f30077dc6438cad5fb3e89d47b2769e
João de Vasconcelos <contact@joao.earth>
2025-01-26 00:57:22 +0000

    Replace video element with GIF in AppNaveganteStartupNaoAumenta2025 component

--- frontend/components/app-navegante/startup/AppNaveganteStartupNaoAumenta2025/index.tsx
+++ frontend/components/app-navegante/startup/AppNaveganteStartupNaoAumenta2025/index.tsx
@@ -3,7 +3,7 @@
 /* * */
 
 import { useEnvironmentContext } from '@/contexts/Environment.context';
-import { Button } from '@mantine/core';
+import { Button, Image } from '@mantine/core';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
@@ -40,9 +40,11 @@ export function AppNaveganteStartupNaoAumenta2025() {
 	return (
 		<div className={styles.container}>
 
-			<video className={styles.video} autoPlay loop muted>
+			{/* <video className={styles.video} autoPlay loop muted>
 				<source src="/assets/app-navegante/startup/welcome/video.mp4" type="video/mp4" />
-			</video>
+			</video> */}
+
+			<Image className={styles.video} src="/assets/app-navegante/startup/welcome/video.gif" />
 
 			<div className={styles.actionsWrapper}>
 				<Button component={Link} href="https://www.navegante.pt/informar/noticias/passes-navegante-nao-aumentam-desde-2019" target="_blank">
