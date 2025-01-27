209e7d030352e9d8b1cfd2bb67d8f8ca4ac4cca7
João de Vasconcelos <contact@joao.earth>
2025-01-27 12:43:50 +0000

    Move metadata to layout instead of page

--- frontend/app/(views)/(website)/lines/page.tsx
+++ frontend/app/(views)/(website)/lines/page.tsx
@@ -1,14 +1,6 @@
 /* * */
 
 import { LinesList } from '@/components/lines/LinesList';
-import { Metadata } from 'next';
-
-/* * */
-
-export const metadata: Metadata = {
-	description: 'Descubra as linhas de autocarro da CMetropolitana.',
-	title: 'CMetropolitana | Linhas',
-};
 
 /* * */
 
--- frontend/app/(views)/(website)/stops/page.tsx
+++ frontend/app/(views)/(website)/stops/page.tsx
@@ -1,14 +1,6 @@
 /* * */
 
 import { StopsList } from '@/components/stops/StopsList';
-import { Metadata } from 'next';
-
-/* * */
-
-export const metadata: Metadata = {
-	description: 'Descubra as paragens de autocarro da CMetropolitana.',
-	title: 'CMetropolitana | Paragens',
-};
 
 /* * */
 
