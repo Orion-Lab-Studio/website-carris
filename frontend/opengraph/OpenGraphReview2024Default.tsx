5fadcdda97bed562afe5db0a689776b41449d0f7
João de Vasconcelos <contact@joao.earth>
2025-01-27 16:18:41 +0000

    Refactor OpenGraph handling: update image rendering logic to conditionally display image based on environment variable

--- frontend/opengraph/OpenGraphReview2024Default.tsx
+++ frontend/opengraph/OpenGraphReview2024Default.tsx
@@ -5,11 +5,13 @@
 export function OpenGraphReview2024Default() {
 	return (
 		<div style={{ backgroundColor: '#fff', display: 'flex', height: '100%', width: '100%' }}>
-			<img
-				alt="Viagem 2024"
-				src={`${process.env.NEXT_PUBLIC_URL}/assets/review-2024/images/viagem2024logo.png`}
-				style={{ height: '100%', objectFit: 'contain', width: '100%' }}
-			/>
+			{process.env.NEXT_PUBLIC_URL && (
+				<img
+					alt="Viagem 2024"
+					src={`${process.env.NEXT_PUBLIC_URL}/assets/review-2024/images/viagem2024logo.png`}
+					style={{ height: '100%', objectFit: 'contain', width: '100%' }}
+				/>
+			)}
 		</div>
 	);
 }
