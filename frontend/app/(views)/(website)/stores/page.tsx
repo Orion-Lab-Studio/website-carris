54aa45379f2fff3d86dac3c907a0f7b6ff1f80e6
João de Vasconcelos <contact@joao.earth>
2025-01-27 12:48:29 +0000

    Refactor OpenGraph handling: implement new image component and update metadata for stores

--- frontend/app/(views)/(website)/stores/page.tsx
+++ frontend/app/(views)/(website)/stores/page.tsx
@@ -1,19 +1,9 @@
 /* * */
 
 import StoresList from '@/components/stores/StoresList';
-import { Metadata } from 'next';
 
 /* * */
 
 export default function Page() {
-	return (
-		<StoresList />
-	);
+	return <StoresList />;
 }
-
-export const metadata: Metadata = {
-	openGraph: {
-		description: 'Lojas e Rede de Agentes',
-		title: 'CMetropolitana - Lojas',
-	},
-};
