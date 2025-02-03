d95ced3a749a8ab4e6554c2085db1ff0b8adc0ec
Skasix00 <brunoalexcastelo@gmail.com>
2025-02-03 17:53:00 +0000

    Started modifying the og:description

--- frontend/app/(views)/(website)/lines/[line_id]/page.tsx
+++ frontend/app/(views)/(website)/lines/[line_id]/page.tsx
@@ -3,6 +3,7 @@
 import { LinesDetail } from '@/components/lines/LinesDetail';
 import { LinesDetailContextProvider } from '@/contexts/LinesDetail.context';
 import { Routes } from '@/utils/routes';
+import { Municipality } from '@carrismetropolitana/api-types/locations';
 import { type Line } from '@carrismetropolitana/api-types/network';
 import { type Metadata } from 'next';
 
@@ -20,18 +21,23 @@ export async function generateMetadata({ params }): Promise<Metadata> {
 	// B. Fetch data
 
 	const allLinesResponse = await fetch(`${Routes.API}/lines`);
+	const allMunicipalitiesResponse = await fetch(`${Routes.API}/locations/localities`);
 	const allLinesData: Line[] = await allLinesResponse.json();
+	const allMunicipalitiesData: Municipality[] = await allMunicipalitiesResponse.json();
 
 	//
 	// C. Transform data
 
 	const lineData = allLinesData.find(item => item.id === line_id);
+	allMunicipalitiesData.find(item => item.id === lineData?.municipality_ids);
+	// const goesTrough = allMunicipalitiesData.find(item => lineData?.municipality_ids?.includes(item.id));
+	// console.log(goesTrough);
 
 	//
 	// D. Render components
 
 	return {
-		description: `Horários em tempo real da linha ${lineData?.short_name}.`,
+		description: `Horários em tempo real da linha ${lineData?.short_name}. Esta linha passa por: `,
 		title: `${lineData?.short_name} | ${lineData?.long_name}`,
 	};
 
