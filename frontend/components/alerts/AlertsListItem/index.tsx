707163f6558cbffc5786ee9639c5cbbe6a59f40a
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-02-12 23:55:02 +0000

    Updates alert URL construction with route constants

--- frontend/components/alerts/AlertsListItem/index.tsx
+++ frontend/components/alerts/AlertsListItem/index.tsx
@@ -13,6 +13,7 @@ import { useTranslations } from 'next-intl';
 import { useMemo } from 'react';
 
 import styles from './styles.module.css';
+import { Routes } from '@/utils/routes';
 
 /* * */
 
@@ -34,11 +35,6 @@ export default function Component({ alertId }: Props) {
 	//
 	// B. Transform data
 
-	const alertHref = useMemo(() => {
-		const currentUrl = window.location.pathname;
-		return `${currentUrl}/${alertId}`;
-	}, [alertId]);
-
 	const simplifiedAlertData = alertsContext.actions.getSimplifiedAlertById(alertId);
 
 	//
@@ -53,8 +49,8 @@ export default function Component({ alertId }: Props) {
 					<AlertActivePeriodEnd date={simplifiedAlertData?.end_date} size="sm" />
 				</div>
 				<p className={styles.description}>{simplifiedAlertData?.description}</p>
-				{simplifiedAlertData?.image_url && <AlertsListItemImageThumbnail alt={simplifiedAlertData?.title} href={alertHref} src={simplifiedAlertData.image_url} />}
-				<Button href={alertHref} icon={<IconArrowUpRight size={16} />} label={t('open')} variant="pill" />
+				{simplifiedAlertData?.image_url && <AlertsListItemImageThumbnail alt={simplifiedAlertData?.title} href={`${Routes.ALERTS.route}/${alertId}`} src={simplifiedAlertData.image_url} />}
+				<Button href={`${Routes.ALERTS.route}/${alertId}`} icon={<IconArrowUpRight size={16} />} label={t('open')} variant="pill" />
 			</Accordion.Panel>
 		</Accordion.Item>
 	);
