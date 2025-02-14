c844579c1a1969160ae9d2708fe616d32e31f914
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-02-14 17:02:43 +0000

    Base Structure Setup

---
+++ frontend/components/inquiry-2024/Inquiry2024Page/index.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+/* * */
+
+import { Inquiry2024Intro } from '@/components/inquiry-2024/Inquiry2024Intro';
+import { Inquiry2024LevelFour } from '@/components/inquiry-2024/Inquiry2024LevelFour';
+import { Inquiry2024LevelOne } from '@/components/inquiry-2024/Inquiry2024LevelOne';
+import { Inquiry2024LevelSix } from '@/components/inquiry-2024/Inquiry2024LevelSix';
+import { Inquiry2024LevelThree } from '@/components/inquiry-2024/Inquiry2024LevelThree';
+import { Inquiry2024LevelTwoA } from '@/components/inquiry-2024/Inquiry2024LevelTwoA';
+import { Inquiry2024LevelTwoB } from '@/components/inquiry-2024/Inquiry2024LevelTwoB';
+import { Inquiry2024LevelTwoC } from '@/components/inquiry-2024/Inquiry2024LevelTwoC';
+import { useQueryState } from 'nuqs';
+
+/* * */
+
+export function Inquiry2024Page() {
+	//
+
+	//
+	// A. Setup variables
+	const [selectedAnchor, setAnchor] = useQueryState('anchor');
+	//
+	// B. Render components
+
+	return (
+		<>
+			<Inquiry2024Intro />
+			<Inquiry2024LevelOne />
+			<Inquiry2024LevelThree />
+			<Inquiry2024LevelTwoA />
+			<Inquiry2024LevelTwoB />
+			<Inquiry2024LevelTwoC />
+			<Inquiry2024LevelFour />
+			<Inquiry2024LevelSix />
+		</>
+	);
+
+	//
+}
---
+++ frontend/opengraph/OpenGraphInquiry2024Default.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+
+/* * */
+
+export function OpenGraphInquiry2024Default() {
+	return (
+		<div style={{ backgroundColor: '#fff', display: 'flex', height: '100%', width: '100%' }}>
+			<img
+				alt="Viagem 2024"
+				src="https://www.carrismetropolitana.pt/assets/review-2024/images/viagem-2024-logo.png"
+				// style={{ height: '100%', objectFit: 'contain', width: '100%' }}
+			/>
+		</div>
+	);
+}
