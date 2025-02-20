04440aa481d96e59309ed3494dbde9aadaedb62c
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-02-20 14:01:57 +0000

    Added Anchor buttons for the new navigation Added 2 new secctions: about open data and the complait metric section

---
+++ frontend/components/metrics/MetricsPageContacts/index.tsx
@@ -0,0 +1,17 @@
+/* * */
+
+/* * */
+
+export function MetricsPageContacts() {
+	//
+	//
+
+	// B. Render components
+	return (
+		<div id="contactsMetrics">
+			<p>Hey dude!</p>
+		</div>
+	);
+
+	//
+}
--- frontend/components/metrics/MetricsPageIntro/index.tsx
+++ frontend/components/metrics/MetricsPageIntro/index.tsx
@@ -3,6 +3,8 @@
 /* * */
 
 import { BackButton } from '@/components/common/BackButton';
+import Button from '@/components/common/Button';
+import { LottiePlayer } from '@/components/common/LottiePlayer';
 import { Section } from '@/components/layout/Section';
 import { Surface } from '@/components/layout/Surface';
 import { useTranslations } from 'next-intl';
@@ -30,6 +32,14 @@ export function MetricsPageIntro() {
 			<Section heading={t('heading')} withGap withPadding>
 				<p className={styles.text}>{t('text_1')}</p>
 				<p className={styles.text}>{t('text_2')}</p>
+				<div className={styles.anchorsWrapper}>
+					<Button href="#aboutOpenData" icon={<LottiePlayer className={styles.lottie} path="/assets/tickets/animations/proxima.json" loop play />} label="Sobre Os Dados Abertos" />
+					<Button href="#passengerMetrics" icon={<LottiePlayer className={styles.lottie} path="/assets/tickets/animations/proxima.json" loop play />}label="Total de Passageiros" />
+					<Button href="#passengerRecords" icon={<LottiePlayer className={styles.lottie} path="/assets/tickets/animations/proxima.json" loop play />} label="Recordes Batidos" />
+					<Button href="#linesMetrics" icon={<LottiePlayer className={styles.lottie} path="/assets/tickets/animations/proxima.json" loop play />} label="Linhas Mais Utilizadas" />
+					<Button href="#serviceMetrics" icon={<LottiePlayer className={styles.lottie} path="/assets/tickets/animations/proxima.json" loop play />} label="Viagens Realizadas" />
+					<Button href="#contactsMetrics" icon={<LottiePlayer className={styles.lottie} path="/assets/tickets/animations/proxima.json" loop play />} label="Contactos" />
+				</div>
 			</Section>
 		</Surface>
 	);
