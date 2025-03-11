4a1529e62a8f5a9a205b01f37f76d993e661007a
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-03-11 11:20:51 +0000

    Added percentage Calculation based on the last weeks passengers total

--- frontend/components/metrics/MetricsPageContactsCardGroup/index.tsx
+++ frontend/components/metrics/MetricsPageContactsCardGroup/index.tsx
@@ -17,11 +17,16 @@ export function MetricsContactsPageCardGroup({ data, filter_type, filter_value, 
 	//
 	console.log('Data received => ', filter_type, filter_value, totalPassengersLastWeek);
 	//
-	// A. Setup variables
+	// A. Fetch data
+
+	const calcPercentage = (value: number, total: number) => {
+		return `${((value / total) * 100).toFixed(3)}%`;
+	};
+
 	const cardData = [
 		{
 			description1: 'total de pedidos de informação',
-			description2: '99.99%', // repplace value with calculated percentage
+			description2: calcPercentage(data.reduce((acc, item) => acc + item.info_requests, 0), totalPassengersLastWeek),
 			description3: 'do total de passageiros transportados na amL na última semana',
 			image: '/assets/complaints/pedidos_info.svg',
 			subheading: 'área metropolitana de Lisboa',
@@ -30,23 +35,20 @@ export function MetricsContactsPageCardGroup({ data, filter_type, filter_value, 
 		},
 		{
 			description1: 'total de reclamações',
-			description2: '99.99%', // repplace value with calculated percentage
+			description2: calcPercentage(data.reduce((acc, item) => acc + item.complaints, 0), totalPassengersLastWeek),
 			description3: 'do total de passageiros transportados na amL na última semana',
 			image: '/assets/complaints/reclamacoes_info.svg',
 			subheading: 'área metropolitana de Lisboa',
 			title: 'Reclamações', value: data.reduce((acc, item) => acc + item.complaints, 0) },
 		{
 			description1: 'total de outro* tipo de contactos',
-			description2: '99.99%', // repplace value with calculated percentage
+			description2: calcPercentage(data.reduce((acc, item) => acc + item.other, 0), totalPassengersLastWeek),
 			description3: 'do total de passageiros transportados na amL na última semana',
 			footer: '*perdidos e achados, sugestões e agradecimentos',
 			image: '/assets/complaints/outros_info.svg',
 			subheading: 'área metropolitana de Lisboa',
 			title: 'Outros*', value: data.reduce((acc, item) => acc + item.other, 0) },
 	];
-	//
-	// B. Fecth data
-
 	//
 	// C. Render domponents
 	return (
