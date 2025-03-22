73b3eefca2f9740d277825a42f72ef39ad1bb4f4
João de Vasconcelos <contact@joao.earth>
2025-03-22 20:50:55 +0000

    Revert "Update dependencies"
    This reverts commit b0baf24346ed331d3147d9df954eadc6a5850f29.

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -9,19 +9,17 @@ import { NextResponse } from 'next/server';
 /* * */
 
 export function middleware(request: NextRequest) {
-	// Ensure the URL is always lowercase.
-	// Transform the URL to lowercase and redirect
-	// to it if the URL is not already lowercase.
-	const lowercasedUrl = new URL(request.nextUrl.pathname.toLowerCase(), request.url);
-	// Set the x-href header to the original URL
-	// to make it easily available in server-side code.
-	const headers = new Headers({ 'x-href': request.nextUrl.href });
-	// Redirect to the lowercase URL if the original URL is not lowercase.
-	if (request.url !== lowercasedUrl.toString()) {
-		return NextResponse.redirect(lowercasedUrl, { headers });
+	const parsedUrl = new URL(request.nextUrl.pathname.toLowerCase(), request.url);
+
+	const headers = new Headers({
+		'x-href': request.nextUrl.href,
+	});
+
+	if (request.url === parsedUrl.toString()) {
+		return NextResponse.next({ headers });
 	}
-	// Continue to the next middleware.
-	return NextResponse.next({ headers });
+
+	return NextResponse.redirect(parsedUrl, { headers });
 }
 
 /* * */
@@ -29,12 +27,12 @@ export function middleware(request: NextRequest) {
 export const config = {
 	matcher: [
 		/*
-		 * Match all request paths except for the ones starting with:
-		 * - api (API routes)
-		 * - _next/static (static files)
-		 * - _next/image (image optimization files)
-		 * - favicon.ico (favicon file)
-		 */
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
 		'/((?!api|_next/static|_next/image|favicon.ico).*)',
 	],
 };
