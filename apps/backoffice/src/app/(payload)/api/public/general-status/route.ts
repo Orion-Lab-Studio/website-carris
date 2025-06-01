4db8a96edde66539a9ecb0bab74186ffc3a598f3
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-01 21:30:47 +0100

    Updated packages

--- apps/backoffice/src/app/(payload)/api/public/general-status/route.ts
+++ apps/backoffice/src/app/(payload)/api/public/general-status/route.ts
@@ -13,7 +13,7 @@ export const GET = async () => {
 	//
 	// Setup Payload and other necessary variables for handling requests.
 
-	const currentDate = Dates.now();
+	const currentDate = Dates.now('utc');
 	const payload = await getPayload({ config: payloadConfig });
 
 	//
