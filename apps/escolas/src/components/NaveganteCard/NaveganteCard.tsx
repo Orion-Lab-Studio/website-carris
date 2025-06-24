c74cef595d1524a982e6daa60bbdd74227e85361
LoudNeo <meliodasgamer077@gmail.com>
2025-06-24 15:44:23 +0100

    fix Mapa coments anda NaveganteCard

--- apps/escolas/src/components/NaveganteCard/NaveganteCard.tsx
+++ apps/escolas/src/components/NaveganteCard/NaveganteCard.tsx
@@ -5,13 +5,17 @@ import styles from './NaveganteCard.module.css';
 import BlackHeader from '../BlackHeader/BlackHeader';
 
 export default function NaveganteCard() {
+	//
+
+	//
+	// render the component
 	return (
 		<div className={styles.caixa}>
 			<BlackHeader text="Sobre o navegante®" />
 			<a href="https://www.carrismetropolitana.pt/cartoes/" rel="noopener noreferrer" target="_blank">
 				<div className={styles.imageContainer}>
 					<div className={styles.imageWrapper}>
-						<Image alt="Passe navegante" height={602} priority src="/images/navegante.png" style={{ height: '100%', width: '100%' }} width={852} />
+						<Image alt="Passe navegante" height={602} src="/images/navegante.png" style={{ height: '100%', width: '100%' }} width={852} priority />
 					</div>
 				</div>
 			</a>
