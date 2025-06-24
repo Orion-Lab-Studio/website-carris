6b818e1fa69c31700e3a4dd348d2ae8a80b84e96
nitpreet18 <nitpreet2006@gmail.com>
2025-06-24 16:42:23 +0100

    AppTopBarLink done

--- apps/escolas/src/components/AppTopBar/AppTopBarLink.tsx
+++ apps/escolas/src/components/AppTopBar/AppTopBarLink.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function AppTopBarLink({ active = false, href, label }: Props) {
 	return (
-		<a className={`${styles.link} ${active ? styles.active : ''}`} href={href}>
+		<a className={`${styles.link} ${active ? styles.active : styles.inactive}`} href={href}>
 			<div className={styles.label}>{label}</div> {}
 			<div className={styles.indicatorWrapper}>
 				<div className={styles.indicatorActive} />
