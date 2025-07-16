dc9d5e87e47f24fa8439ec89d734e868f6a27949
victormicco <victor.oficial093@gmail.com>
2025-07-15 21:51:16 -0300

    feat: Add ArrabicaHeader

---
+++ apps/frontend/src/app/(views)/(website)/arrabida/page.tsx
@@ -0,0 +1,9 @@
+/* * */
+
+import { ArrabidaPage } from '@/components/arrabida/ArrabidaPage';
+
+/* * */
+
+export default function Page() {
+	return <ArrabidaPage />;
+} 
---
+++ apps/frontend/src/components/arrabida/ArrabidaPage/index.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+/* * */
+
+import { ArrabidaHeader } from '@/components/arrabida/ArrabidaHeader';
+import { Surface } from '@/components/layout/Surface';
+import { Accordion, Image, Text, Title } from '@mantine/core';
+import { useTranslations } from 'next-intl';
+
+import styles from './styles.module.css';
+
+/* * */
+
+export function ArrabidaPage() {
+	//
+
+	//
+	// A. Setup variables
+
+	const t = useTranslations('arrabida.ArrabidaPage');
+
+	//
+	// B. Render components
+
+	return (
+		<>
+			<Surface forceOverflow>
+				<div className={styles.headerContainer}>
+					<Title order={1} className={styles.title}>
+						{t('title')}
+					</Title>
+					<Text className={styles.subtitle}>
+						{t('subtitle')}
+					</Text>
+				</div>
+			</Surface>
+
+			<Surface forceOverflow>
+				<Accordion>
+					<Accordion.Item value="section-1">
+						<Accordion.Control>{t('sections.section-1.title')}</Accordion.Control>
+						<Accordion.Panel>
+							<Text className={styles.text}>{t('sections.section-1.text-1')}</Text>
+							<Text className={styles.text}>{t('sections.section-1.text-2')}</Text>
+						</Accordion.Panel>
+					</Accordion.Item>
+
+					<Accordion.Item value="section-2">
+						<Accordion.Control>{t('sections.section-2.title')}</Accordion.Control>
+						<Accordion.Panel>
+							<Text className={styles.text}>{t('sections.section-2.text-1')}</Text>
+							<Text className={styles.text}>{t('sections.section-2.text-2')}</Text>
+						</Accordion.Panel>
+					</Accordion.Item>
+
+					<Accordion.Item value="section-3">
+						<Accordion.Control>{t('sections.section-3.title')}</Accordion.Control>
+						<Accordion.Panel>
+							<Text className={styles.text}>{t('sections.section-3.text-1')}</Text>
+							<Text className={styles.text}>{t('sections.section-3.text-2')}</Text>
+						</Accordion.Panel>
+					</Accordion.Item>
+				</Accordion>
+			</Surface>
+		</>
+	);
+} 
