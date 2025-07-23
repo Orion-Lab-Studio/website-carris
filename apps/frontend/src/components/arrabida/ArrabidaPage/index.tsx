079e6bc237a54cb7689eef7550df5df73e55e449
victormicco <victor.oficial093@gmail.com>
2025-07-23 16:28:49 -0300

    feat: integrate ArrabidaWay section with new components and translations

--- apps/frontend/src/components/arrabida/ArrabidaPage/index.tsx
+++ apps/frontend/src/components/arrabida/ArrabidaPage/index.tsx
@@ -2,14 +2,9 @@
 
 /* * */
 
-import { ArrabidaHeader } from '@/components/arrabida/ArrabidaHeader';
-import { Surface } from '@/components/layout/Surface';
-import { Accordion, Image, Text, Title } from '@mantine/core';
-import { useTranslations } from 'next-intl';
-
-import styles from './styles.module.css';
 import { Arrabida365 } from '../Arrabida365';
 import { ArrabidaAbout } from '../ArrabidaAbout';
+import { ArrabidaWay } from '../ArrabidaWay';
 
 /* * */
 
@@ -19,43 +14,14 @@ export function ArrabidaPage() {
 	//
 	// A. Setup variables
 
-	const t = useTranslations('arrabida.ArrabidaPage');
-
 	//
 	// B. Render components
 
 	return (
 		<>
 			<Arrabida365 />
+			<ArrabidaWay />
 			<ArrabidaAbout />
-
-			<Surface forceOverflow>
-				<Accordion>
-					<Accordion.Item value="section-1">
-						<Accordion.Control>{t('sections.section-1.title')}</Accordion.Control>
-						<Accordion.Panel>
-							<Text className={styles.text}>{t('sections.section-1.text-1')}</Text>
-							<Text className={styles.text}>{t('sections.section-1.text-2')}</Text>
-						</Accordion.Panel>
-					</Accordion.Item>
-
-					<Accordion.Item value="section-2">
-						<Accordion.Control>{t('sections.section-2.title')}</Accordion.Control>
-						<Accordion.Panel>
-							<Text className={styles.text}>{t('sections.section-2.text-1')}</Text>
-							<Text className={styles.text}>{t('sections.section-2.text-2')}</Text>
-						</Accordion.Panel>
-					</Accordion.Item>
-
-					<Accordion.Item value="section-3">
-						<Accordion.Control>{t('sections.section-3.title')}</Accordion.Control>
-						<Accordion.Panel>
-							<Text className={styles.text}>{t('sections.section-3.text-1')}</Text>
-							<Text className={styles.text}>{t('sections.section-3.text-2')}</Text>
-						</Accordion.Panel>
-					</Accordion.Item>
-				</Accordion>
-			</Surface>
 		</>
 	);
 } 
